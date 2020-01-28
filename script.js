21bd62849edc3d04f8efc7bddd96e4fbc4f46d32
Tayfun Sur <tayfunsur@gmail.com>
2020-01-28 11:46:20 +0300

    created the grid structure

--- script.js
+++ script.js
@@ -3,5 +3,18 @@ const Player = (name) => {
 };
 
 const Gameboard = (() => {
-
+    const r1c1 = document.getElementById(1); 
+    const r1c2 = document.getElementById(2); 
+    const r1c3 = document.getElementById(3); 
+    const r2c1 = document.getElementById(4); 
+    const r2c2 = document.getElementById(5); 
+    const r2c3 = document.getElementById(6);
+    const r3c1 = document.getElementById(7); 
+    const r3c2 = document.getElementById(8); 
+    const r3c3 = document.getElementById(9);
+    gameboardArray = [[r1c1,r1c2,r1c3],[r2c1,r2c2,r2c2],[r3c1,r3c2,r3c3]];
 })();
+
+const flowControl = (() => {
+
+});

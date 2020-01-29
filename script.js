852cd17f10ebf7733e5547184fb5775b0652c809
Tayfun Sur <tayfunsur@gmail.com>
2020-01-29 17:02:28 +0300

    whole code has changed since the unbeatable ai

--- script.js
+++ script.js
@@ -1,189 +1,152 @@
-const Control = (() => {
-    const startButton = document.querySelector('.start_button');
-    const goFirst = document.querySelector('.playfirst_button');
-    const goSecond = document.querySelector('.playsecond_button');
-    startButton.addEventListener('click', clean);
-    goFirst.addEventListener('click', changeTurn1);
-    goSecond.addEventListener('click', changeTurn2);
-    let turn = "player";
+let gameBoard;
+let huPlayer ='O';
+let aiPlayer = 'X';
+const winCombos =[
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 4, 8],
+  [6, 4, 2],
+  [2, 5, 8],
+  [1, 4, 7],
+  [0, 3, 6]
+];
 
-    goFirst.style.backgroundColor = "grey";
+const gameboards = document.querySelectorAll('.gameboard');
+startGame();
 
-    function clean(){
-        flowControl.controlTheFlow();
-        flowControl.myArray = [0,1,2,3,4,5,6,7,8];
-        flowControl.winner.textContent = "Welcome!";
-        flowControl.grids.forEach(grid => {
-            console.log("before "+grid.textContent);
-            grid.textContent = "";
-            console.log("after " +grid.textContent);
-        });
-        
-        if (goFirst.style.backgroundColor == "grey"){
-            turn = "player";
-        } else if (goSecond.style.backgroundColor == "grey") {
-            turn = "player2";
-        }
-        startButton.disabled = true;
-        startButton.className = "inactive";
-        goFirst.disabled = true;
-        goSecond.disabled = true;
+function symbol(sym){
+  huPlayer = sym;
+  aiPlayer = sym==='O' ? 'X' :'O';
+  gameBoard = Array.from(Array(9).keys());
+  for (let i = 0; i < gameboards.length; i++) {
+    gameboards[i].addEventListener('click', turnClick, false);
+  }
+  if (aiPlayer === 'X') {
+    turn(bestSpot(),aiPlayer);
+  }
+  document.querySelector('.symbol').style.display = "none";
+}
 
-   
-        
-        // flowControl.controlTheFlow();
+function startGame() {
+  document.querySelector('.endgame').style.display = "none";
+  document.querySelector('.endgame .text').innerText ="";
+  document.querySelector('.symbol').style.display = "block";
+  for (let i = 0; i < gameboards.length; i++) {
+    gameboards[i].innerText = '';
+    gameboards[i].style.removeProperty('background-color');
+  }
+}
+
+function turnClick(square) {
+  if (typeof gameBoard[square.target.id] ==='number') {
+    turn(square.target.id, huPlayer);
+    if (!checkWin(gameBoard, huPlayer) && !checkTie())  
+      turn(bestSpot(), aiPlayer);
+  }
+}
+
+function turn(squareId, player) {
+  gameBoard[squareId] = player;
+  document.getElementById(squareId).innerHTML = player;
+  let gameWon = checkWin(gameBoard, player);
+  if (gameWon) gameOver(gameWon);
+  checkTie();
+}
+
+function checkWin(board, player) {
+  let plays = board.reduce((a, e, i) => (e === player) ? a.concat(i) : a, []);
+  let gameWon = null;
+  for (let [index, win] of winCombos.entries()) {
+    if (win.every(elem => plays.indexOf(elem) > -1)) {
+      gameWon = {index: index, player: player};
+      break;
     }
+  }
+  return gameWon;
+}
 
-    function changeTurn1(){
-        console.log("now turn is: "+turn);
-        if (turn == "player2") {
-        flowControl.changeTurn();
-        }
-        goFirst.style.backgroundColor = "grey";
-        goSecond.style.backgroundColor = "#c62d1f";
-        turn = "player";
+function gameOver(gameWon){
+  for (let index of winCombos[gameWon.index]) {
+    document.getElementById(index).style.backgroundColor = 
+      gameWon.player === huPlayer ? "blue" : "red";
+  }
+  for (let i=0; i < gameboards.length; i++) {
+    gameboards[i].removeEventListener('click', turnClick, false);
+  }
+  declareWinner(gameWon.player === huPlayer ? "You win!" : "You lose");
+}
+
+function declareWinner(who) {
+  document.querySelector(".endgame").style.display = "block";
+  document.querySelector(".endgame .text").innerText = who;
+}
+function emptySquares() {
+  return gameBoard.filter((elm, i) => i===elm);
+}
+  
+function bestSpot(){
+  return minimax(gameBoard, aiPlayer).index;
+}
+  
+function checkTie() {
+  if (emptySquares().length === 0){
+    for (gameboard of gameboards) {
+      gameboard.style.backgroundColor = "green";
+      gameboard.removeEventListener('click',turnClick, false);
     }
-    function changeTurn2(){
-        console.log("now turn is: "+turn);
-        if (turn == "player") {
-            flowControl.changeTurn();
-        }
-        goSecond.style.backgroundColor = "grey";
-        goFirst.style.backgroundColor = "#c62d1f";
-        turn = "player2";
-    }
+    declareWinner("Tie game");
+    return true;
+  } 
+  return false;
+}
 
-    return{turn,goFirst,goSecond,startButton};
-})();
-
-const Player = (name,marker) => {
-    return {name, marker};
-};
-
-const Gb = (() => {
-    const r1c1 = document.getElementById(0); 
-    const r1c2 = document.getElementById(1); 
-    const r1c3 = document.getElementById(2); 
-    const r2c1 = document.getElementById(3); 
-    const r2c2 = document.getElementById(4); 
-    const r2c3 = document.getElementById(5);
-    const r3c1 = document.getElementById(6); 
-    const r3c2 = document.getElementById(7); 
-    const r3c3 = document.getElementById(8);
- 
-    let gbArray = [[r1c1,r1c2,r1c3],[r2c1,r2c2,r2c2],[r3c1,r3c2,r3c3]];
-    return {gbArray, r1c1, r1c2, r1c3, r2c1, r2c2, r2c3, r3c1, r3c2, r3c3};
-
-})();
-
-const flowControl = (function controlTheFlow() {
-    console.log("flowkontrol yuklendi");
-    const grids = [...document.querySelectorAll('.gameboard')];
-    const winner = document.querySelector('.winner');
-    winner.textContent = "Welcome!";
-    document.body.insertBefore(winner, document.body.firstChild);
-    grids.forEach(grid => {
-        grid.addEventListener('click', play);
-    });
-
-    const player1 = Player("ahmet", "X");
-    const player2 = Player("player2", "O");
+function minimax(newBoard, player) {
+  var availSpots = emptySquares(newBoard);
+  
+  if (checkWin(newBoard, huPlayer)) {
+    return {score: -10};
+  } else if (checkWin(newBoard, aiPlayer)) {
+    return {score: 10};
+  } else if (availSpots.length === 0) {
+    return {score: 0};
+  }
+  
+  var moves = [];
+  for (let i = 0; i < availSpots.length; i ++) {
+    var move = {};
+    move.index = newBoard[availSpots[i]];
+    newBoard[availSpots[i]] = player;
     
-
- function changeTurn(){
-        if (Control.turn == "player2"){ 
-            Control.turn = "player";
-            console.log("turn has changed to: "+Control.turn);
-        }else if(Control.turn == "player"){
-            Control.turn = "player2";
-            console.log("turn has changed to: "+Control.turn);
-        }
-     }
-    
-
-    myArray = [0,1,2,3,4,5,6,7,8];
-    
-    function play(){
-        
-        let cpuGrid;
-        let condition = false;
-        console.log("id"+this.id);
-        console.log("myarray = " + myArray);
-        if (this.textContent == ""){
-            if (Control.turn == "player"){
-                this.textContent = player1.marker;
-                winningCondition();
-              
-                myArray.splice(myArray.indexOf(Number(this.id)),1);
-                console.log(myArray);
-            }
-        
-            while (condition == false){
-                cpuGrid = Math.floor(Math.random()*myArray.length);
-                console.log("secilen cpu grid indexi: "+cpuGrid);
-                
-                if (myArray.length != 0){ 
-                    if (grids[myArray[cpuGrid]].textContent == ""){                    
-                        condition = true;     
-                        grids[myArray[cpuGrid]].textContent = player2.marker;
-                        myArray.splice(myArray.indexOf(myArray[cpuGrid]),1);
-                        console.log(myArray);
-                        condition = false;
-                        break;           
-                    } else {
-                        condition = false;
-                    }
-                } else {
-                    break;
-                }
-            }
-        }
+    if (player === aiPlayer)
+      move.score = minimax(newBoard, huPlayer).score;
+    else
+       move.score =  minimax(newBoard, aiPlayer).score;
+    newBoard[availSpots[i]] = move.index;
+    if ((player === aiPlayer && move.score === 10) || (player === huPlayer && move.score === -10))
+      return move;
+    else 
+      moves.push(move);
+  }
+  
+  let bestMove, bestScore;
+  if (player === aiPlayer) {
+    bestScore = -1000;
+    for(let i = 0; i < moves.length; i++) {
+      if (moves[i].score > bestScore) {
+        bestScore = moves[i].score;
+        bestMove = i;
+      }
     }
-
-    function winningCondition(){
-        function changeCond(){
-            Control.startButton.disabled = false;
-            Control.startButton.className = "start_button red";
-            Control.goFirst.disabled = false;
-            Control.goSecond.disabled = false;
-            if (Control.goFirst.style.backgroundColor == "grey"){
-                Control.turn = "player";
-            } else if (Control.goFirst.style.backgroundColor == "grey") {
-                Control.turn = "cpu";
-            }
-        };
-        if ((Gb.r1c1.textContent == Gb.r1c2.textContent) && (Gb.r1c1.textContent == Gb.r1c3.textContent) && (Gb.r1c1.textContent != "")){
-            winner.textContent = "we have a winner!";
-            changeCond();
-        } else if ((Gb.r2c1.textContent == Gb.r2c2.textContent) && (Gb.r2c1.textContent == Gb.r2c3.textContent) && (Gb.r2c1.textContent != "")){
-            winner.textContent = "we have a winner!"; 
-            changeCond();          
-        } else if ((Gb.r3c1.textContent == Gb.r3c2.textContent) && (Gb.r3c1.textContent == Gb.r3c3.textContent) && (Gb.r3c1.textContent != "")){
-            winner.textContent = "we have a winner!";  
-            changeCond();         
-        } else if ((Gb.r1c1.textContent == Gb.r2c1.textContent) && (Gb.r1c1.textContent == Gb.r3c1.textContent) && (Gb.r1c1.textContent != "")){
-            winner.textContent = "we have a winner!";            
-            changeCond();
-        } else if ((Gb.r1c2.textContent == Gb.r2c2.textContent) && (Gb.r1c2.textContent == Gb.r3c2.textContent) && (Gb.r3c2.textContent != "")){
-            winner.textContent = "we have a winner!";   
-            changeCond();         
-        } else if ((Gb.r1c3.textContent == Gb.r2c3.textContent) && (Gb.r1c3.textContent == Gb.r3c3.textContent) && (Gb.r1c3.textContent != "")){
-            winner.textContent = "we have a winner!";  
-            changeCond();          
-        } else if ((Gb.r1c1.textContent == Gb.r2c2.textContent) && (Gb.r1c1.textContent == Gb.r3c3.textContent) && (Gb.r1c1.textContent != "")){
-            winner.textContent = "we have a winner!";  
-            changeCond();         
-        } else if ((Gb.r1c3.textContent == Gb.r2c2.textContent) && (Gb.r1c3.textContent == Gb.r3c1.textContent) && (Gb.r1c3.textContent != "")){
-            winner.textContent = "we have a winner!"; 
-            changeCond();           
-        } else if ((Gb.r1c1.textContent != "") && (Gb.r1c2.textContent != "") && (Gb.r1c3.textContent != "") && (Gb.r2c1.textContent != "") && (Gb.r2c2.textContent != "") && (Gb.r2c3.textContent != "") && (Gb.r3c1.textContent != "") && (Gb.r3c2.textContent != "") && (Gb.r3c3.textContent != "")){
-            winner.textContent = "berabere";
-            changeCond();
-        }
-        
-        
+  } else {
+      bestScore = 1000;
+      for(let i = 0; i < moves.length; i++) {
+      if (moves[i].score < bestScore) {
+        bestScore = moves[i].score;
+        bestMove = i;
+      }
     }
-    
-    return {grids, winner, controlTheFlow,changeTurn,play,myArray};
-
-})();
+  }
+  
+  return moves[bestMove];
+}
